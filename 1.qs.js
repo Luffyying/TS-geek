2d39cf2a0b6f33f1936874a00e83e705e7319cd4
wangwanying <lufei_ying@sina.com>
2019-09-10 20:24:15 +0800

    udpate

---
+++ 1.qs.js
@@ -0,0 +1,8 @@
+export const parser = str => {
+    const obj = {};
+    str.replace(/([^=&]+)=([^=&]+)/g, function() {
+      obj[arguments[1]] = arguments[2];
+    });
+    return obj;
+  };
+  // name=luffy&age=90&time=1993   -> {name:luffy,age:90,time:1993}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TS-geek
-test
+> babelrc 中presets(预设) 告诉babel要转换的源码使用了哪些新的语法特性，是一组plugins的集合
